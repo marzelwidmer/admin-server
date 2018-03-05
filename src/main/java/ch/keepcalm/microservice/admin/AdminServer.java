0cfb8e6a87dbe1a584790afb156e895e8cf51fc9
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-05 21:35:44 +0100

    spring-boot-admin 2.0.0-SNAPSHOT connections

--- src/main/java/ch/keepcalm/microservice/admin/AdminServer.java
+++ src/main/java/ch/keepcalm/microservice/admin/AdminServer.java
@@ -1,17 +1,18 @@
 package ch.keepcalm.microservice.admin;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 
+@EnableGlobalAuthentication
+@EnableDiscoveryClient
+
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
-@EnableGlobalAuthentication
-@EnableDiscoveryClient
 public class AdminServer {
 
     public static void main(String[] args) {
